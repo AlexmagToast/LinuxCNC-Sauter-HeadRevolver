d55fc59acb6ce3726df4fd16978c7d320bf07c3b
Alexander Richter <alex@3dator.com>
2023-07-23 21:35:27 +0200

    tested and working on my machine

--- HeadRevolver.py
+++ HeadRevolver.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3.9
-import time, hal
-#	LubeDude for LinuxCNC
+import time, hal , re
+
+#	HeadRevolver for LinuxCNC
 #	By Alexander Richter, info@theartoftinkering.com 2022
 #	Please consider supporting me on Patreon.com/theartoftinkering
-
+#
+#   This Program supports the Hydraulic Turret by Sauter. 
+#	It was included in the Weiler Primus CNC and runs entirely by one hydraulik piston. More detail can be seen here: 
+#   https://www.youtube.com/watch?v=d5WtskW0o68
+#
+#	This script waits for "trequest" to go high. It will then read "toolnumber" and look it up in the Tool table. 
+# 	There it looks for "R" and the Number 1-4 representing one of each Turret sides in the comment section. 
+#	Then the current Turrent pos is checked and the Hydraulic will cycle until the required Turret Pos is reached. 
+#	Lastly the "toolchanged" Pin goes High, which will indicate to LinuxCNC that the Turret is ready. 
 #
 #	This program is free software; you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -18,7 +27,7 @@ import time, hal
 #	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
 
 #Config
-File = "/home/alex/linuxcnc/configs/sim.axis"
+File = "/home/primuscnc/linuxcnc/configs/WeilerPrimusCNC/tool.tbl" #change this to point to your tool table file
 
 pistonreturn = 1
 #HAL Setup
@@ -30,11 +39,16 @@ c.newpin("pos1", hal.HAL_BIT, hal.HAL_IN)
 c.newpin("pos2", hal.HAL_BIT, hal.HAL_IN)
 c.newpin("pos3", hal.HAL_BIT, hal.HAL_IN)
 c.newpin("pos4", hal.HAL_BIT, hal.HAL_IN)
+c.newpin("trequest", hal.HAL_BIT, hal.HAL_IN)
+
+c.newpin("toolnumber", hal.HAL_S32, hal.HAL_IN)
+
 
 #Outputs
 c.newpin("coilA", hal.HAL_BIT, hal.HAL_OUT)
 c.newpin("coilB", hal.HAL_BIT, hal.HAL_OUT)
 
+c.newpin("toolchanged", hal.HAL_BIT, hal.HAL_OUT)
 
 c.newpin("fault", hal.HAL_BIT, hal.HAL_OUT)
 c.newpin("reset", hal.HAL_BIT, hal.HAL_IN)
@@ -58,66 +72,70 @@ def getPosition(): #returns Revolver Position returns 0 while turning
         return 0
 
 
-def extract_nbr(input_str):
-    if input_str is None or input_str == '':
-        return 0
-
-    out_number = ''
-    for ele in input_str:
-        if ele.isdigit():
-            out_number += ele
-    return int(out_number) 
-
 def defineToolpos(toolnbr):  #finds Toolorientation on Revolver
+
+    regex = r".*T(\d+).*;.*R(\d+)"
+    fileinhalt = ""
+
     with open(File) as f:
-        for line in f:
-            data = line.strip()
-            data = data.split(" ",1)
-            if toolnbr == extract_nbr(data[0]):
-                data = data[1].split("R",1)
-                return extract_nbr(data[1][0])
-                break
+        fileinhalt = f.read()
+
+    matches = re.finditer(regex, fileinhalt, re.MULTILINE)
+
+    for matchNum, match in enumerate(matches, start=1):
+        
+        #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = m3atchNum, start = match.start(), end = match.end(), match = match.group()))
+        T = int(match.group(1))
+        R = int(match.group(2))
+        
+        if T == toolnbr:
+            return (R)
 
 def wait(counter):#wait for piston to return
     return counter + pistonreturn < time.time()
 
 while True:
+	time.sleep(0.1)
 	try:
-		c.debug = State
-		nextTool = hal.get_value("iocontrol.0.tool-prep-number")
-	
+		#c.debug = defineToolpos(c.toolnumber)
 		if c.hydraulik == 0: c.fault = 1
 		else:c.fault = 0
+		c.toolchanged = 0
 
-
-		if hal.get_value("iocontrol.0.tool-prepare"):
-			preppedTool = defineToolpos(nextTool)
-			hal.set_p("iocontrol.0.tool-prepared","1")
-
-		if hal.get_value("iocontrol.0.tool-change") == 1:
-			if preppedTool != getPosition():
+		if c.trequest == 1 and c.toolchanged == 0:
+			time.sleep(0.1)
+			
+			newTool = defineToolpos(c.toolnumber)
+			if newTool != getPosition():
+				c.toolchanged = 0
+				c.debug = State
 				while State == 0:
-					c.CoilA = 1
-					c.CoilB = 0
+					c.coilA = 1
+					c.coilB = 0
 					if getPosition() == 0:
 						State = 1
+					time.sleep(0.1)
 				c.debug = State
 				while State == 1:
+					time.sleep(1.25)                    
 					if getPosition != 0:
 						State = 2
 				c.debug = State
 				while State == 2:
-					c.CoilA = 0
-					c.CoilB = 1
-					if wait(time.time()) == 1:
-						State = 3
+					c.coilA = 0
+					c.coilB = 1
+					time.sleep(1.25)
+					State = 3
+					
 				c.debug = State
+				
 				if State == 3:
-					c.CoilA = 0
-					c.CoilB = 0
+					c.coilA = 0
+					c.coilB = 0
 					State = 0
-					hal.set_p("iocontrol.0.tool-changed","1")
 					c.debug = State
+			if newTool == getPosition():
+				c.toolchanged = 1 
 	except:
 		pass
 
