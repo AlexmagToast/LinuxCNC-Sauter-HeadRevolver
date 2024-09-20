9bca9af42ae6a3b3ee642788a579df926b5eeabe
Alexander Richter <alex@3dator.com>
2024-09-20 17:27:52 +0200

    New updates, not stable

--- HeadRevolver.py
+++ HeadRevolver.py
@@ -104,25 +104,35 @@ while True:
 
 		if c.trequest == 1 and c.toolchanged == 0:
 			time.sleep(0.1)
-			
+
 			newTool = defineToolpos(c.toolnumber)
 			if newTool != getPosition():
 				c.toolchanged = 0
 				c.debug = State
+				cycles = 0
 				while State == 0: #Standard Position
+					cycles +=1
 					c.coilA = 1
 					c.coilB = 0
 					if getPosition() == 0:
 						State = 1
+					c.debug = cycles
+					if cycles > 50: # return to Start State if the Toolhead fails to reach Position after 5 seconds
+						c.coilA = 0
+						c.coilB = 1
+						time.sleep(2.25)
+						cycles = 0
 					time.sleep(0.1)
-					c.debug = State
 				while State == 1: #Start turning to new Position and wait for reaching new Position
 					start_time = time.time()  
 					time.sleep(1.25)         
 					if getPosition() != 0: #proceed to next State if new Position is reached
 						State = 2
 					elif time.time() - start_time > 5: # return to Start State if the Toolhead fails to reach Position after 5 seconds
-						State = 0
+							c.coilA = 0
+							c.coilB = 1
+							time.sleep(2.25)
+							State = 1
 					c.debug = State
 				while State == 2:
 					c.coilA = 0
@@ -138,7 +148,8 @@ while True:
 					State = 0
 					c.debug = State
 			if newTool == getPosition():
-				c.toolchanged = 1 
+			
+					c.toolchanged = 1 
 	except:
 		pass
 
